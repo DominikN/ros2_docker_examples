6deed44c67f40a704eab6791faaa231c853c9ba1
dominikn <dominik.nwk@gmail.com>
2021-06-26 23:36:09 +0200

    working on xml parser

--- eg8/dev1/dds_update/dds_xml_updater.py
+++ eg8/dev1/dds_update/dds_xml_updater.py
@@ -1,9 +1,26 @@
+import os 
 import time
+import xml.etree.ElementTree as ET
+
 print("Hello World!")
 
+tree = ET.parse(os.path.join('/dds', 'cyclonedds.xml'))
+root = tree.getroot()
 
+for peer in root.findall('.//{https://cdds.io/config}Peer'):
+    print(peer.tag, peer.attrib)
+    print(peer.attrib['address'])
+    
 while True:
     time.sleep(2)
+    
+    # remove all Peers from xml
+    for peer in root.findall('.//{https://cdds.io/config}Peer'):
+        root.remove(peer)
+    
     hosts = open('/etc/hosts','r')
     for line in hosts:
-        print(line.split()[1:])
+        if(line.split()[2:6] == ['#', 'managed', 'by', 'Husarnet']):
+            print(line.split()[0])  # Husarnet IPv6 address
+            print(line.split()[1])  # Husarnet Hostname
+            ET.SubElement('.//{https://cdds.io/config}Peers', 'Peer', address=line.split()[0])
