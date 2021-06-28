d720052475e899b6de2c9f34029d258dc292b79e
dominikn <dominik.nwk@gmail.com>
2021-06-28 13:27:52 +0200

    xml work in progress

--- eg8/dev1/dds_update/dds_xml_updater.py
+++ eg8/dev1/dds_update/dds_xml_updater.py
@@ -3,8 +3,10 @@ import time
 import xml.etree.ElementTree as ET
 
 print("Hello World!")
+xml_file = os.path.join('/dds', 'cyclonedds.xml')
+ET.register_namespace("", "https://cdds.io/config")
 
-tree = ET.parse(os.path.join('/dds', 'cyclonedds.xml'))
+tree = ET.parse(xml_file)
 root = tree.getroot()
 
 for peer in root.findall('.//{https://cdds.io/config}Peer'):
@@ -12,15 +14,20 @@ for peer in root.findall('.//{https://cdds.io/config}Peer'):
     print(peer.attrib['address'])
     
 while True:
-    time.sleep(2)
+    ET.dump(root)
     
     # remove all Peers from xml
     for peer in root.findall('.//{https://cdds.io/config}Peer'):
-        root.remove(peer)
+        print("peer to remove:")
+        print(peer.tag, peer.attrib)
+        root.find('.//{https://cdds.io/config}Peers').remove(peer)
     
     hosts = open('/etc/hosts','r')
     for line in hosts:
         if(line.split()[2:6] == ['#', 'managed', 'by', 'Husarnet']):
             print(line.split()[0])  # Husarnet IPv6 address
             print(line.split()[1])  # Husarnet Hostname
-            ET.SubElement('.//{https://cdds.io/config}Peers', 'Peer', address=line.split()[0])
+            ET.SubElement(root.find('.//{https://cdds.io/config}Peers'), 'Peer', address=line.split()[0])
+            with open(xml_file, 'w') as f:
+                tree.write(f, encoding='unicode')
+    time.sleep(5)
