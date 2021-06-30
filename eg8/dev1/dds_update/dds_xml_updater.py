9c7374d70f574ddaba46a52bd426440dba5ec864
dominikn <dominik.nwk@gmail.com>
2021-06-30 18:24:49 +0200

    dds xml updater almost done

--- eg8/dev1/dds_update/dds_xml_updater.py
+++ eg8/dev1/dds_update/dds_xml_updater.py
@@ -2,20 +2,60 @@ import os
 import time
 import xml.etree.ElementTree as ET
 import xmltodict
+import json
+import pprint
+# from collections import OrderedDict
 
-
+pp = pprint.PrettyPrinter(indent=4)
 print("Hello World!")
 xml_file = os.path.join('/dds', 'cyclonedds.xml')
 
-with open(xml_file) as fd:
-    doc = xmltodict.parse(fd.read())
-
-print(doc)
-
 while True:
-    print(doc['CycloneDDS']['Domain']['Discovery']['Peers'])
-    print('number of peers' + str(len(doc['CycloneDDS']['Domain']['Discovery']['Peers']['Peer'])))
+    with open(xml_file, "r") as fd:
+        raw_xml = fd.read()
+        doc = xmltodict.parse(raw_xml)
+        myjson = json.loads(json.dumps(doc))
+        fd.close()
+    
+    print('\r\n[0] Peers:\r\n')
+    pp.pprint(raw_xml)
+    
+    print('\r\n[1] Peers:\r\n')
+    pp.pprint(myjson)
+    
+    print('\r\n[2] Peers:\r\n')
+    hosts = open('/etc/hosts','r')
+    myjson['CycloneDDS']['Domain']['Discovery']['Peers']['Peer'] = []
+    for line in hosts:
+        if(line.split()[2:6] == ['#', 'managed', 'by', 'Husarnet']):
+            print(line.split()[0])  # Husarnet IPv6 address
+            print(line.split()[1])  # Husarnet Hostname
+            myjson['CycloneDDS']['Domain']['Discovery']['Peers']['Peer'].append({'@address': line.split()[0]})
+    pp.pprint(myjson)
+    
+    print('\r\n[4] Peers:\r\n')
+    new_raw_xml = xmltodict.unparse(myjson, pretty=True)
+
+    print(new_raw_xml)
+    
+    with open(xml_file, "w") as fd:
+        fd.write(new_raw_xml)
+        fd.close()
+    
     time.sleep(5)
+    
+    # print('\r\nPeers:\r\n')
+    # peers = doc['CycloneDDS']['Domain']['Discovery']['Peers']
+    # peers['Peer'].append(OrderedDict[("@address", "jakis-ipv6")])
+    # # peers['Peer']['@address']=123
+    # print(peers)
+    
+    # print('\r\nPeers:\r\n')
+    # del peers['Peer']
+    # print(peers)
+    
+    # print('number of peers: ' + str(len(doc['CycloneDDS']['Domain']['Discovery']['Peers']['Peer'])))
+    # time.sleep(5)
 
 ET.register_namespace("", "https://cdds.io/config")
 
